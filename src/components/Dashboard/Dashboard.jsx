5816afa6ad153d2c5f843e188a77b3178fc4fbcd
HuseynovRamin <122551952+HuseynovRamin@users.noreply.github.com>
2025-06-27 00:53:38 +0400

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -16,6 +16,7 @@ export default function Dashboard() {
     },
   ];
 
+  
   return (
     <main className="flex-1 p-8 bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Ваши клубы</h1>
