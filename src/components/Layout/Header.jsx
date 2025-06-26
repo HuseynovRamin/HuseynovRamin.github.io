fd82cd44a85c9b13dfac0621dcf2e61a50552e74
HuseynovRamin <122551952+HuseynovRamin@users.noreply.github.com>
2025-06-27 00:31:53 +0400

    Create Header.jsx

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-white shadow p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold text-blue-600">EduClubs</h1>
+      {/* You can add profile menu or logout button here */}
+    </header>
+  );
+}
