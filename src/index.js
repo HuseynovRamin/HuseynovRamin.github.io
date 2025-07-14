43870b888342140e7791b4973c56e2e2cb0b277d
HuseynovRamin <122551952+HuseynovRamin@users.noreply.github.com>
2025-07-14 20:28:08 +0400

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Make sure to have Tailwind CSS setup her
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
